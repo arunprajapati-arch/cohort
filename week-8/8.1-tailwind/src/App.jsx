5c935c003b58040da0b25a93d2d1627a279dee09
Arun Prajapati <arunprajapati.mail@gmail.com>
2024-06-18 15:07:11 +0530

    tailwind done

---
+++ week-8/8.1-tailwind/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { RevenueCard } from './components/RevenueCard'
+
+function App() {
+
+  return (
+    <>
+    <div className=' grid grid-cols-4'>
+    <RevenueCard title = {"Amount Pending"} amount={"92,111"} orderCount={13} />
+    </div>
+     
+    </>
+  )
+}
+
+export default App
